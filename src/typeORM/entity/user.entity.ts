3b7ddf461a177db9794cf148789b132372cbf76e
Samuel <alu01013595262ull.edu.es>
2024-05-11 18:31:10 +0100

    feat: JWT generator on tjhe user

--- src/typeORM/entity/user.entity.ts
+++ src/typeORM/entity/user.entity.ts
@@ -1,6 +1,7 @@
 import { ObjectId } from "mongodb";
 import { Entity, ObjectIdColumn, Column, BeforeInsert, Unique } from "typeorm";
-import * as bcrypt from 'bcrypt';
+import * as bcrypt from "bcrypt";
+import * as jwt from "jsonwebtoken";
 
 @Entity()
 @Unique(["email"])
@@ -20,5 +21,25 @@ export class User {
   @BeforeInsert()
   async hashPassword() {
     this.password = await bcrypt.hash(this.password, 10);
+  }
+
+  generateJWT() {
+    const today = new Date();
+    const expirationDate = new Date(today);
+    expirationDate.setDate(today.getDate() + 60);
+
+    const secret = process.env.JWT_SECRET;
+    if (!secret) {
+      throw new Error("JWT Secret not defined");
+    }
+
+    return jwt.sign(
+      {
+        email: this.email,
+        id: this._id,
+        exp: parseInt((expirationDate.getTime() / 1000).toString(), 10),
+      },
+      secret
+    );
   }
 }
