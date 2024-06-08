e3b1c2153fbf614968971a5bf50cceb00258a381
Samuel <alu01013595262ull.edu.es>
2024-06-08 19:09:17 +0100

    feat: all tracker time range operation

--- src/express/routes/trackerData.ts
+++ src/express/routes/trackerData.ts
@@ -23,6 +23,75 @@ router.get("/", async (req, res) => {
   }
 });
 
+// Operación de obtención de todos los beacons que se encuentran dentro de un rango de fecha
+router.get("/allTrackerDataByDateRange", async (req, res) => {
+  const trackerDataRepository = getRepository(TrackerData);
+
+  try {
+    const startDate = req.query.startDate;
+    const endDate = req.query.endDate;
+
+    let options: { where: { time: any } } = { where: { time: {} } };
+
+    if (startDate && endDate) {
+      const start = new Date(startDate as string).toISOString().split(".")[0];
+      const end = new Date(endDate as string).toISOString().split(".")[0];
+
+      options.where.time = {
+        $gte: start,
+        $lte: end,
+      };
+    }
+
+    const allTrackerDataByDateRange = await trackerDataRepository.find(options);
+
+    if (!allTrackerDataByDateRange) {
+      return res.status(404).json({ message: "No documents" });
+    }
+    res.json(allTrackerDataByDateRange).status(200);
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ message: "Internal server error" });
+  }
+});
+
+// Operación de obtención de todos los beacons que se encuentran dentro de una fecha específica en unas horas en concreto
+router.get("/allTrackerDataByHourRange", async (req, res) => {
+  const trackerDataRepository = getRepository(TrackerData);
+
+  try {
+    const date = req.query.date;
+    const startHour = req.query.startHour;
+    const endHour = req.query.endHour;
+
+    let options: { where: { time?: any } } = { where: {} };
+
+    if (date && startHour && endHour) {
+      const start = new Date(`${date as string}T${startHour as string}Z`)
+        .toISOString()
+        .split(".")[0];
+      const end = new Date(`${date as string}T${endHour as string}Z`)
+        .toISOString()
+        .split(".")[0];
+
+      options.where.time = {
+        $gte: start,
+        $lte: end,
+      };
+    }
+
+    const allTrackerDataByHourRange = await trackerDataRepository.find(options);
+
+    if (!allTrackerDataByHourRange) {
+      return res.status(404).json({ message: "No documents" });
+    }
+    res.json(allTrackerDataByHourRange).status(200);
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ message: "Internal server error" });
+  }
+});
+
 // Operación de obtención de todos los identificadores de manera única de los beacons
 router.get("/ids", async (req, res) => {
   const trackerDataRepository = getRepository(TrackerData);
@@ -54,11 +123,13 @@ router.get("/:id", async (req, res) => {
     const startHour = req.query.startHour;
     const endHour = req.query.endHour;
 
-    let options: { where: { droneId: string; time?: any } } = { where: { droneId: id } };
+    let options: { where: { droneId: string; time?: any } } = {
+      where: { droneId: id },
+    };
 
     if (startDate && endDate) {
-      const start = new Date(startDate as string).toISOString().split('.')[0];
-      const end = new Date(endDate as string).toISOString().split('.')[0];
+      const start = new Date(startDate as string).toISOString().split(".")[0];
+      const end = new Date(endDate as string).toISOString().split(".")[0];
 
       options.where.time = {
         $gte: start,
@@ -67,11 +138,17 @@ router.get("/:id", async (req, res) => {
     }
 
     if (specificDate) {
-      const start = new Date(`${specificDate as string}T${startHour as string}Z`).toISOString().split('.')[0];
-      const end = new Date(`${specificDate as string}T${endHour as string}Z`).toISOString().split('.')[0];
-      console.log(start)
-      console.log(end)
-    
+      const start = new Date(
+        `${specificDate as string}T${startHour as string}Z`
+      )
+        .toISOString()
+        .split(".")[0];
+      const end = new Date(`${specificDate as string}T${endHour as string}Z`)
+        .toISOString()
+        .split(".")[0];
+      console.log(start);
+      console.log(end);
+
       options.where.time = {
         $gte: start,
         $lte: end,
